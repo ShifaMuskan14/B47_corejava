3c97d626744baaf09cc229e1fea02e077243d2c2
shifamuskan GitHub <shifa.shif@gmail.com>
2023-03-21 14:33:34 +0530

    First commit

---
+++ C2TC.VVIT/src/com/tns/packagetwo/SubClass.java
@@ -0,0 +1,17 @@
+package com.tns.packagetwo;
+
+import com.tns.packageone.Base;
+
+public class SubClass 
+{
+  public static void main(String[] args) 
+  {
+	Base b=new Base();
+	b.methodPublic();
+	//b.methodDefault();
+	//b.methodProtected();
+	//b.methodPrivate();
+	
+	
+  }
+}
