f812abf75bca2144dc7ccab35e44caaacfe8b134
shifamuskan GitHub <shifa.shif@gmail.com>
2023-03-24 16:33:07 +0530

    First commit

---
+++ C2TC.VVIT/src/com/tns/staticprogram/EmployeeExecutor.java
@@ -0,0 +1,17 @@
+package com.tns.staticprogram;
+
+public class EmployeeExecutor 
+{
+  public static void main(String[] args) 
+  {
+	Employee ob1=new Employee();
+	System.out.println(Employee.companyName);
+	Employee ob=new Employee("Shifa",123);
+	System.out.println(ob);
+	
+	Employee ob2=new Employee();
+	System.out.println(Employee.companyName);
+	ob=new Employee("Muskan",456);
+	System.out.println(ob);
+  }
+}
