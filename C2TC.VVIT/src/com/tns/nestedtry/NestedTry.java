f10410665af5b66210eb05368926fdb4b92e228c
shifamuskan GitHub <shifa.shif@gmail.com>
2023-04-03 16:55:32 +0530

    First Commit

--- C2TC.VVIT/src/com/tns/nestedtry/NestedTry.java
+++ C2TC.VVIT/src/com/tns/nestedtry/NestedTry.java
@@ -4,17 +4,40 @@ package com.tns.nestedtry;
 
 public class NestedTry 
 {
-  int nestedtry(int x,int y)
-  {
-	  int z=0;
-	  try
-	  {
-		z=x/y;
-	  }
-	  catch(ArithmeticException ar)
-	  {
-		  System.out.println();
-	  }
-	return z;
-  }
+	int nestedtry(int x,int y) 
+	{
+		String str="hello";
+		str=null;
+		int[] arr= {1,2,3};
+		int z=0;
+		try 
+		{
+			z=x/y;
+			try 
+			{
+				System.out.println(arr[y]);
+				try 
+				{
+					int slength=str.length();
+					System.out.println("string lenght"+slength);
+				}
+				catch(NullPointerException np) {
+					System.out.println("null pointer exception"+np);
+				}
+			}
+			catch(ArrayIndexOutOfBoundsException a) {
+				System.out.println("array index out of bound"+a);
+			}
+			
+		}
+		catch(ArithmeticException e) 
+		{
+			System.out.println("division by 0 error"+e);
+		}
+		finally 
+		{ 
+		  System.out.println("finally block"); 
+		}
+		return z;
+	}
 }
