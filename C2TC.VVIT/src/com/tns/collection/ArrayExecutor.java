2856a4a12ace088d0879c173316ee0378d47f36c
shifamuskan GitHub <shifa.shif@gmail.com>
2023-04-07 16:27:51 +0530

    Array

---
+++ C2TC.VVIT/src/com/tns/collection/ArrayExecutor.java
@@ -0,0 +1,13 @@
+package com.tns.collection;
+
+import java.util.ArrayList;
+
+public class ArrayExecutor 
+{
+  public static void main(String[] args) 
+  {
+	//Creating an ArrayList
+	ArrayList<String> vvit=new ArrayList<String>();
+	System.out.println(vvit);
+  }
+}
