613633b9799b01bf4166ccdd5fb53c62886c2b89
shifamuskan GitHub <shifa.shif@gmail.com>
2023-04-07 16:32:35 +0530

    Array

--- C2TC.VVIT/src/com/tns/collection/ArrayExecutor.java
+++ C2TC.VVIT/src/com/tns/collection/ArrayExecutor.java
@@ -8,6 +8,13 @@ public class ArrayExecutor
   {
 	//Creating an ArrayList
 	ArrayList<String> vvit=new ArrayList<String>();
+	System.out.println(vvit);   //displaying the ArrayList
+	vvit.add("Shifa");
+	vvit.add("Moufeeka");
+	vvit.add(null);
+	vvit.add("Iqra");
+	System.out.println(vvit);
+	vvit.remove(1);
 	System.out.println(vvit);
   }
 }
